dae572ad28fda9353d9ccfefaede0525e59d0757
Diego Ramirez <merle2187580@gmail.com>
2025-09-21 18:31:57 -0600

    Feat:addprint

---
+++ app.py
@@ -0,0 +1,10 @@
+import socket
+print("Sexo")
+
+hostname = socket.gethostname()
+print(f"Hostname: {hostname}")
+
+ipaddress = socket.gethostbyname(hostname)
+print(f"IP: {ipaddress}")
+
+print("Nuevo")
